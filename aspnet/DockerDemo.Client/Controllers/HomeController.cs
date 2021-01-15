9a8d93fabed5668806d08d55df77421f227dd639
Nar <rohini.smi2010@gmail.com>
2021-01-15 16:50:56 -0600

    adding testing

--- aspnet/DockerDemo.Client/Controllers/HomeController.cs
+++ aspnet/DockerDemo.Client/Controllers/HomeController.cs
@@ -13,6 +13,10 @@ namespace DockerDemo.Client.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public HomeController()
+        {
+            
+        }
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
